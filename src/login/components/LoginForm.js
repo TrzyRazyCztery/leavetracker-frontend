34d3a21fd1bac6f5fd298d37135a4b6fc550eece
TrzyRazyCztery <3x4to9@gmail.com>
2018-04-28 13:34:27 +0200

    Basic look for login form added

--- src/login/components/LoginForm.js
+++ src/login/components/LoginForm.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
-//import validatedInput from "../../shared/components/validatedInput"
+import validatedInput from "../../shared/components/validatedInput"
 import colors from "../../variables/colors";
 import Button from "material-ui/Button";
+import ValidatedInput from "../../shared/components/ValidatedInput";
 class LoginForm extends Component {
+
   state = {
-    login: "",
-    password: ""
+    loginData:{
+      login: "",
+      password: ""
+    }
   };
+
+  buttonStyle = { backgroundColor: colors["mainColor"] };
 
   handleChange = name => event => {
     this.setState({
@@ -15,15 +21,36 @@ class LoginForm extends Component {
   };
 
   render() {
+    const {loginData} = this.state;
+    const {errors} = this.props;
     return (
-      <Button
-        style={{ backgroundColor: colors["mainColor"] }}
-        variant="raised"
-        color="primary"
-      >
-        {" "}
-        LOGIN{" "}
-      </Button>
+        <div>
+          <ValidatedInput
+              name="email"
+              label="Email"
+              type="text"
+              hasErrors={}
+              errors={}
+              handleChange={this.handleChange}
+              value={loginData.login}
+          />
+          <ValidatedInput
+              name="password"
+              label="Password"
+              type="password"
+              hasErrors={}
+              errors={}
+              handleChange={this.handleChange}
+              value={loginData.password}
+          />
+          <Button
+              style = {this.buttonStyle}
+              variant="raised"
+              color="primary"
+          >
+            {" LOGIN "}
+          </Button>
+        </div>
     );
   }
 }
