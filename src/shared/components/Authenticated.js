63bb83b4eb4fa089808f89fac7deb80c5d955c19
TrzyRazyCztery <3x4to9@gmail.com>
2018-05-03 14:54:17 +0200

    Added HoC to switch between Authenticated and Unauthenticated components

---
+++ src/shared/components/Authenticated.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { getAuthenticatedUser } from "../../reducers/authorizationDataReducer";
+import { connect } from "react-redux";
+
+const Authenticated = ({ children, user }) => (!!user ? children : null);
+
+const mapStateToProps = state => ({
+  user: getAuthenticatedUser(state)
+});
+
+export default connect(mapStateToProps)(Authenticated);
---
+++ src/shared/components/Unauthenticated.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { getAuthenticatedUser } from "../../reducers/authorizationDataReducer";
+import { connect } from "react-redux";
+
+const Unauthenticated = ({ children, user }) => (!!user ? null : children);
+
+const mapStateToProps = state => ({
+  user: getAuthenticatedUser(state)
+});
+
+export default connect(mapStateToProps)(Unauthenticated);
