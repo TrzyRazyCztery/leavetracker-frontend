b0e03fc937ef213903ced902294f9969c437f4c6
TrzyRazyCztery <3x4to9@gmail.com>
2018-05-04 11:29:13 +0200

    Rewrote storage adapter to use ES2016 syntax

--- src/utils/adapters.js
+++ src/utils/adapters.js
@@ -1,7 +1,13 @@
 import store from "store";
 
 export const storageAdapter = {
-  store: (name, data) => store.set(name, data),
-  load: name => store.get(name),
-  remove: name => store.remove(name)
+  store(name, data) {
+    store.set(name, data);
+  },
+  load(name) {
+    store.get(name);
+  },
+  remove(name) {
+    store.remove(name);
+  }
 };
