bbd6328dbe2c14939a8bcb06d024831ed19bf11f
TrzyRazyCztery <3x4to9@gmail.com>
2018-05-04 11:20:38 +0200

    removed unnecessary null in connect

--- src/shared/components/Notifications.js
+++ src/shared/components/Notifications.js
@@ -36,4 +36,4 @@ const mapStateToProps = state => ({
   notification: getNotification(state)
 });
 
-export default connect(mapStateToProps, null)(Notifications);
+export default connect(mapStateToProps)(Notifications);
