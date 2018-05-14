fe3cfa4671428eea30237382949b09a8a809afdf
TrzyRazyCztery <3x4to9@gmail.com>
2018-05-14 16:58:27 +0200

    rewrote backend for better maintenance in the future. Added api for requests

---
+++ src/helpers/fakeBackend/rootBackend.js
@@ -0,0 +1,45 @@
+import { usersData } from 'helpers/fakeData/usersData';
+import { desksData } from 'helpers/fakeData/desksData';
+import {
+  requestsData,
+  requestStatuses,
+  requestTypes
+} from 'helpers/fakeData/requestsData';
+import _ from 'lodash';
+import { desksApi } from 'helpers/fakeBackend/desks';
+import { usersApi } from 'helpers/fakeBackend/users';
+import { requestsApi } from 'helpers/fakeBackend/requests';
+
+let users = usersData;
+let desks = desksData;
+let requests = requestsData;
+let requestUtils = {
+  requestStatuses,
+  requestTypes
+};
+
+const APIs = {
+  users: {
+    url: '/users',
+    api: usersApi(users)
+  },
+  desks: {
+    url: '/desks',
+    api: desksApi(desks)
+  },
+  requests: {
+    url: '/requests',
+    api: requestsApi(requests, requestUtils)
+  }
+};
+
+export const configureFakeBackend = () => {
+  window.fetch = function(url, opts) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const validAPI = _.find(APIs, api => url.includes(api.url));
+        !!validAPI ? validAPI.api(opts, url, resolve, reject) : null;
+      }, 500);
+    });
+  };
+};
---
+++ src/helpers/fakeData/desksData.js
@@ -0,0 +1,22 @@
+export const desksData = {
+  '42': {
+    id: 42,
+    ownerId: 1
+  },
+  '43': {
+    id: 43,
+    ownerId: 3
+  },
+  '44': {
+    id: 44,
+    ownerId: 4
+  },
+  '45': {
+    id: 45,
+    ownerId: 5
+  },
+  '46': {
+    id: 46,
+    ownerId: null
+  }
+};
