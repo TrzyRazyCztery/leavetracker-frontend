fb0f36bd5bc2ec661ba89f2140d71c0dae76697f
TrzyRazyCztery <3x4to9@gmail.com>
2018-05-17 23:05:01 +0200

    changed import names to avoid renaming them during combining reducers.

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,20 +1,20 @@
 import { combineReducers } from 'redux';
-import registerUserReducer from 'reducers/registerUserReducer';
-import logInUserReducer from 'reducers/logInUserReducer';
-import notificationReducer from 'reducers/notificationReducer';
-import authorizationDataReducer from 'reducers/authorizationDataReducer';
-import deskReducer from 'reducers/deskReducer';
-import userReducer from 'reducers/userReducer';
-import requestReducer from 'reducers/requestReducer';
+import registerData from 'reducers/registerUserReducer';
+import logInData from 'reducers/logInUserReducer';
+import notifications from 'reducers/notificationReducer';
+import authorizationData from 'reducers/authorizationDataReducer';
+import desks from 'reducers/deskReducer';
+import users from 'reducers/userReducer';
+import requests from 'reducers/requestReducer';
 
 const rootReducer = combineReducers({
-  registerData: registerUserReducer,
-  logInData: logInUserReducer,
-  notifications: notificationReducer,
-  authorizationData: authorizationDataReducer,
-  desks: deskReducer,
-  users: userReducer,
-  requests: requestReducer
+  registerData,
+  logInData,
+  notifications,
+  authorizationData,
+  desks,
+  users,
+  requests
 });
 
 export default rootReducer;
