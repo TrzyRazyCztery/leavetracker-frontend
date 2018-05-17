36395f8b7e26bb1e025e9cee18ffcdd67e84b41a
TrzyRazyCztery <3x4to9@gmail.com>
2018-05-17 23:07:39 +0200

    improved code style using pretty

--- src/reducers/requestReducer.js
+++ src/reducers/requestReducer.js
@@ -1,28 +1,18 @@
-import {createActionNamespace} from 'utils/actions';
-import _ from 'lodash';
+import { createActionNamespace } from "utils/actions";
+import _ from "lodash";
 
-const requestAction = createActionNamespace('request');
+const requestAction = createActionNamespace("request");
 
-export const LOAD_REQUESTS_SUCCESS = requestAction(
-  'LOAD_REQUESTS_SUCCESS'
-);
-export const CREATE_REQUEST_SUCCESS = requestAction(
-  'CREATE_REQUESTS_SUCCESS'
-);
-export const CREATE_REQUEST_FAILED = requestAction(
-  'CREATE_REQUEST_FAILED'
-);
-export const REMOVE_REQUEST_SUCCESS = requestAction(
-  'REMOVE_REQUEST_SUCCESS'
-);
-export const SAVED_FORM_ERRORS = requestAction(
-  'SAVED_FORM_ERRORS'
-);
+export const LOAD_REQUESTS_SUCCESS = requestAction("LOAD_REQUESTS_SUCCESS");
+export const CREATE_REQUEST_SUCCESS = requestAction("CREATE_REQUESTS_SUCCESS");
+export const CREATE_REQUEST_FAILED = requestAction("CREATE_REQUEST_FAILED");
+export const REMOVE_REQUEST_SUCCESS = requestAction("REMOVE_REQUEST_SUCCESS");
+export const SAVED_FORM_ERRORS = requestAction("SAVED_FORM_ERRORS");
 export const LOAD_REQUEST_UTILS_SUCCESS = requestAction(
-  'LOAD_REQUEST_UTILS_SUCCESS'
+  "LOAD_REQUEST_UTILS_SUCCESS"
 );
 export const CHANGE_REQUEST_STATUS_SUCCESS = requestAction(
-  'CHANGE_REQUEST_STATUS_SUCCESS'
+  "CHANGE_REQUEST_STATUS_SUCCESS"
 );
 
 export const getErrors = state => state.desks.errors;
@@ -51,14 +41,27 @@ const requestReducer = (state = initialState, action) => {
         }
       };
     case LOAD_REQUEST_UTILS_SUCCESS:
-      return {...state, requestStatusesList: action.requestUtils.requestStatuses, requestTypesList: action.requestUtils.requestTypes};
+      return {
+        ...state,
+        requestStatusesList: action.requestUtils.requestStatuses,
+        requestTypesList: action.requestUtils.requestTypes
+      };
     case SAVED_FORM_ERRORS:
-      return {...state, errors: action.errors };
+      return { ...state, errors: action.errors };
     case REMOVE_REQUEST_SUCCESS:
-      return {...state, requestsList: _.omit(state.requestsList, action.requestId)};
+      return {
+        ...state,
+        requestsList: _.omit(state.requestsList, action.requestId)
+      };
     case CHANGE_REQUEST_STATUS_SUCCESS:
-      return {...state, requestsList: {...state.requestsList, [action.updatedRequest.id]: action.updatedRequest}};
-      default:
+      return {
+        ...state,
+        requestsList: {
+          ...state.requestsList,
+          [action.updatedRequest.id]: action.updatedRequest
+        }
+      };
+    default:
       return state;
   }
 };
