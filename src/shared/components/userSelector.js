181c80a5fc66b85b2cece0d28d8e91e290161064
TrzyRazyCztery <3x4to9@gmail.com>
2018-05-21 15:20:09 +0200

    Added possibility to style User Select Component

--- src/shared/components/userSelector.js
+++ src/shared/components/userSelector.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import { MenuItem } from 'material-ui/Menu';
-import Select from 'material-ui/Select';
-import _ from 'lodash'
+import React from "react";
+import { MenuItem } from "material-ui/Menu";
+import Select from "material-ui/Select";
+import {map, compact} from "lodash";
 
-const UserSelector = ({ users, handleUserChange, value }) => (
-  <Select value={value} onChange={handleUserChange}>
-    {_.map(users, (user, index) => (
+const UserSelector = ({ users, handleUserChange, value, style = {} }) => (
+  <Select style={style} value={value} onChange={handleUserChange}>
+    {map(users, (user, index) => (
       <MenuItem value={user.id} key={index}>
-        {user.name + ' ' + user.surname}
+        {compact([user.name, user.surname]).join(' ')}
       </MenuItem>
     ))}
   </Select>
