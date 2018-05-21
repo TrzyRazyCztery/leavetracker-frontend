1eb2e0c53c18e1d493410c14b9c40416ff60d63e
TrzyRazyCztery <3x4to9@gmail.com>
2018-05-21 15:17:13 +0200

    deleted unused user information page

--- src/userInformation/components/UserInformation.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import 'userInformation/styles/userInformation.css';
-
-const UserInformation = () => {
-  return <div>USER INFORMATION PAGE</div>;
-};
-
-export default UserInformation;
