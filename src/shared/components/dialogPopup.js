55a32dd7d174f8037252e9511b856de9217fc9e0
TrzyRazyCztery <3x4to9@gmail.com>
2018-06-04 16:45:20 +0200

    Extracted dialog popup as separate component, because it's used similar way many times in application

---
+++ src/shared/components/dialogPopup.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import Button from 'material-ui/Button'
+import Dialog, {DialogTitle,DialogContent} from 'material-ui/Dialog'
+
+
+class DialogPopup extends Component {
+  state = { activeDialog: false };
+
+  handleInfoOpen = () => {
+    this.setState({ activeDialog: true });
+  };
+
+  handleInfoClose = () => {
+    this.setState({ activeDialog: false });
+  };
+
+  render() {
+    const {children, title, buttonStyle} = this.props;
+    return (
+      <div className="dialog-popup">
+        <Button style = {buttonStyle} onClick={this.handleInfoOpen}>Available desks</Button>
+        <Dialog open={this.state.activeDialog} onClose={this.handleInfoClose}>
+          <DialogTitle>
+            {title}
+          </DialogTitle>
+          <DialogContent>
+            {children}
+          </DialogContent>
+        </Dialog>
+      </div>
+    );
+  }
+}
+
+export default DialogPopup
