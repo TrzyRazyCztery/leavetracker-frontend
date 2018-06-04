019a64033b681d950b86f284232586fc963dc31d
TrzyRazyCztery <3x4to9@gmail.com>
2018-06-04 17:17:42 +0200

    Removed calculations from leave type calendar and stats card components regarding to extracting all request calculations into separate utils file

--- src/employeeInformation/components/leaveTypeStatsCard.js
+++ src/employeeInformation/components/leaveTypeStatsCard.js
@@ -1,31 +1,9 @@
 import React from "react";
 import { leaveTypeCardStyle } from "employeeInformation/styles/styles";
 import { listOfBusinessDaysBetweenDates } from "utils/dates";
-import { reduce } from "lodash";
 import StatsCard from "shared/components/statsCard";
+import {numberOfDaysInYearForLeaveTypeId} from "utils/requests";
 
-const filterRequests = leaveTypeId => requests =>
-  requests.filter(
-    request =>
-      request.requestTypeId === leaveTypeId && request.requestStatusId === 1
-  );
-
-const filterBusinessDaysByYear = year => businessDays =>
-  businessDays.filter(day => day.year() === year);
-
-const numberOfDaysInYearForLeaveTypeId = (requests, leaveTypeId, year) => {
-  const requestFilter = filterRequests(leaveTypeId);
-  const yearFilter = filterBusinessDaysByYear(year);
-  return reduce(
-    requestFilter(requests),
-    (counter, request) =>
-      counter +
-      yearFilter(
-        listOfBusinessDaysBetweenDates(request.startDate, request.endDate)
-      ).length,
-    0
-  );
-};
 
 const LeaveTypeStatsCard = ({ userRequests, selectedYear, leaveTypeId }) => {
   const cardStyle = leaveTypeCardStyle[leaveTypeId];
